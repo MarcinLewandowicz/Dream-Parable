043194e8e4679f0120d6b3e978c436cad00829be
MarcinLewandowicz <131770609+MarcinLewandowicz@users.noreply.github.com>
2023-05-26 12:44:40 +0200

    Spawner completed

--- Assets/Scripts/AgentSpanwer.cs
+++ Assets/Scripts/AgentSpanwer.cs
@@ -4,20 +4,72 @@ using UnityEngine;
 
 public class AgentSpanwer : MonoBehaviour
 {
+    public static AgentSpanwer instance;
+
     [Header("Agent settins")]
     [SerializeField] private GameObject agent;
     [Range(3, 5)]
     [SerializeField] private int startAgentsNumber = 3;
-    [Range(3, 6)]
-    [SerializeField] private float agentsSpawnInterval = 3f;
+    [Range(2, 6)]
+    [SerializeField] private float agentSpawnInterval = 3f;
+    private float timer = 0f;
     [SerializeField] private int maxAgentsNumber = 30;
-    public int currentAgentsNumber;
+    [SerializeField] private int currentAgentsNumber;
 
     [Space(10)]
     [Header("Spawn points settings")]
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private float spawnPointsThreshold = 0.5f;
 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        SpawnStartingAgents();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+
+        if(currentAgentsNumber == maxAgentsNumber) { return; }
+        if (timer >= agentSpawnInterval)
+        {
+            SpawnAgent();
+            timer = 0f;
+        }
+    }
+
+    public void DecreaseAgentsNumber()
+    {
+        currentAgentsNumber--;
+    }
+
+    private void SpawnAgent()
+    {
+        Instantiate(agent, GetRandomEmptySpawnPoint().position, Quaternion.identity);
+        currentAgentsNumber++;
+    }
+
+    private void SpawnStartingAgents()
+    {
+        while (currentAgentsNumber < startAgentsNumber)
+        {
+            SpawnAgent();
+        }
+    }
 
     private List<Transform> GetEmptySpawnPoints()
     {
@@ -31,6 +83,12 @@ public class AgentSpanwer : MonoBehaviour
             }
         }
         return emptySpawnPoints;
+    }
+
+    private Transform GetRandomEmptySpawnPoint()
+    {
+        Transform emptySpawnPoint = GetEmptySpawnPoints()[Random.Range(0, GetEmptySpawnPoints().Count)];
+        return emptySpawnPoint;
     }
 
     private void OnDrawGizmos()
